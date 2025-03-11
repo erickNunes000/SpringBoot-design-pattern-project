3ee7d74ea09ec0ee0159a8ed0468effb497a0cc7
Erick Nunes <erickFernandes000@gmail.com>
2025-03-11 00:14:56 -0300

    add getbyid delete put post methods

--- src/main/java/DP_Spring/SpringProject/controller/ClienteController.java
+++ src/main/java/DP_Spring/SpringProject/controller/ClienteController.java
@@ -3,9 +3,11 @@ package DP_Spring.SpringProject.controller;
 import DP_Spring.SpringProject.model.Cliente;
 import DP_Spring.SpringProject.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class ClienteController {
@@ -15,9 +17,37 @@ public class ClienteController {
 
     @GetMapping("/")
     @ResponseBody
-    public String hello(){
+    public ResponseEntity<String>hello(){
         Cliente cli = new Cliente("Erico", "Rua JJ Doutado");
+        Cliente cli2 = new Cliente("maria", "Rua 04");
         repo.save(cli);
-        return "Cliente criado e salvo: " + cli.getNome();
+        repo.save(cli2);
+        return ResponseEntity.ok("Cliente criado e salvo: " + cli.getNome());
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente>adicionar(@RequestBody Cliente cliente){ //requestbody deve ser usado a objetos
+       // Cliente cli = new Cliente(nome, endereco);
+        Cliente cli = repo.save(cliente);
+        return ResponseEntity.ok(cli);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> findById(@PathVariable Long id){
+        Optional<Cliente> cli = repo.findById(id);
+        return ResponseEntity.ok(cli.get());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente>atualizar(@PathVariable Long id ,@RequestBody Cliente cliente){
+        Optional<Cliente> cli = repo.findById(id);
+        if(cli.isPresent()){
+            cli.get().setNome(cliente.getNome());
+            cli.get().setEndereco(cliente.getEndereco());
+            Cliente atualizado =  repo.save(cli.get());
+            return ResponseEntity.ok(atualizado);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
     }
 }
